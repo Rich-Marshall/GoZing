9a385f46c4e5cb3df42651bcc1f2857bfd845fa2
Your Namerich-marshall <p16189101@my365.dmu.ac.uk>
2021-03-23 13:02:02 +0000

    running most code through beautifier

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -1,27 +1,23 @@
-window.onload = function () {
-    if (localStorage.getItem("hasCodeRunBefore") === null) {
-
-      $(function() {
-          toastr.options = {
-            "closeButton": true,
-            "debug": false,
-            "positionClass": "toast-bottom-right",
-            "hideDuration": "1000",
-            "timeOut": "0",
-            "extendedTimeOut": "0",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-          }
-          toastr.options.onclick = function () {
-
-              location.href = '/privacy#scroll-cookies';
-
-          };
-          toastr.info("<p'>Cookies help us deliver our services. By using our services, you agree to our use of cookies. <a href='/privacy#scroll-cookies' target='_new'>More details</a></p>");
-        });
-
-        localStorage.setItem("hasCodeRunBefore", true);
-    }
+window.onload = function() {
+	if(localStorage.getItem("hasCodeRunBefore") === null) {
+		$(function() {
+			toastr.options = {
+				"closeButton": true,
+				"debug": false,
+				"positionClass": "toast-bottom-right",
+				"hideDuration": "1000",
+				"timeOut": "0",
+				"extendedTimeOut": "0",
+				"showEasing": "swing",
+				"hideEasing": "linear",
+				"showMethod": "fadeIn",
+				"hideMethod": "fadeOut"
+			}
+			toastr.options.onclick = function() {
+				location.href = '/privacy#scroll-cookies';
+			};
+			toastr.info("<p'>Cookies help us deliver our services. By using our services, you agree to our use of cookies. <a href='/privacy#scroll-cookies' target='_new'>More details</a></p>");
+		});
+		localStorage.setItem("hasCodeRunBefore", true);
+	}
 }
